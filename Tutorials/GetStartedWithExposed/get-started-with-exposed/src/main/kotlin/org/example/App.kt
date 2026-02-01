6a4dc0b98fe1437216a2249b4444b3e425445910
mon0mon <mon0mon@naver.com>
2026-02-01 14:46:40 +0900

    #1 Get Started With Exposed feat: 테이블 생성 및 쿼리

--- Tutorials/GetStartedWithExposed/get-started-with-exposed/src/main/kotlin/org/example/App.kt
+++ Tutorials/GetStartedWithExposed/get-started-with-exposed/src/main/kotlin/org/example/App.kt
@@ -1,7 +1,36 @@
 package org.example
 
+import org.jetbrains.exposed.v1.core.count
 import org.jetbrains.exposed.v1.jdbc.Database
+import org.jetbrains.exposed.v1.jdbc.SchemaUtils
+import org.jetbrains.exposed.v1.jdbc.insert
+import org.jetbrains.exposed.v1.jdbc.select
+import org.jetbrains.exposed.v1.jdbc.selectAll
+import org.jetbrains.exposed.v1.jdbc.transactions.transaction
 
 fun main() {
     Database.connect("jdbc:h2:mem:test", driver = "org.h2.Driver")
+
+    transaction {
+        SchemaUtils.create(Tasks)
+
+        val taskId = Tasks.insert {
+            it[title] = "Learn Exposed"
+            it[description] = "Go through the Get started with Exposed"
+        } get Tasks.id
+
+        val secondTaskId = Tasks.insert {
+            it[title] = "Read the Hobbit"
+            it[description] = "Read the first two chapters of the Hobbit"
+            it[isCompleted] = true
+        } get Tasks.id
+
+        println("Created new tasks with ids $taskId and $secondTaskId")
+
+        Tasks.select(Tasks.id.count(), Tasks.isCompleted)
+            .groupBy(Tasks.isCompleted)
+            .forEach { println("${it[Tasks.isCompleted]}: ${it[Tasks.id.count()]}") }
+
+        println("Remaining tasks: ${Tasks.selectAll().toList()}")
+    }
 }
