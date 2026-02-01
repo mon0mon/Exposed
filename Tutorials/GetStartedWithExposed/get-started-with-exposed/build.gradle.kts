93b0262e6d22d586e8635d074630e52a2c873f16
mon0mon <mon0mon@naver.com>
2026-02-01 14:20:58 +0900

    #1 Get Started With Exposed init: 신규 프로젝트 생성

---
+++ Tutorials/GetStartedWithExposed/get-started-with-exposed/build.gradle.kts
@@ -0,0 +1,22 @@
+plugins {
+    kotlin("jvm") version "2.3.0"
+}
+
+group = "xyz.mon0mon"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(kotlin("test"))
+}
+
+kotlin {
+    jvmToolchain(21)
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
